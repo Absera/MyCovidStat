ec115e6d99cbce328ff0f2344f6a325c84671651
Absera <50250863+Absera@users.noreply.github.com>
2020-05-13 16:22:54 +0300

    'tc'

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,5 +1,9 @@
 # variables that will be imported in the main app
 
-token = '829399417:AAEZKjtE05GTzEdbKmmeAjTny8JXxkFHcUw'
-username = 't.me/myCovidStatbot'
-url = 'https://mycovidstat.herokuapp.com/'
+# token = '829399417:AAEZKjtE05GTzEdbKmmeAjTny8JXxkFHcUw'
+# username = 't.me/myCovidStatbot'
+# url = 'https://mycovidstat.herokuapp.com/'
+
+bot_token = "829399417:AAEZKjtE05GTzEdbKmmeAjTny8JXxkFHcUw"
+bot_user_name = "t.me/myCovidStatbot"
+URL = "https://mycovidstat.herokuapp.com/"

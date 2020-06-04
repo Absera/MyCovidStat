893a0b0776bb929e25edef6f1d20f994a032cbb8
Absera Temesgen <50250863+Absera@users.noreply.github.com>
2020-06-04 06:01:27 +0300

    Update credentials.py

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,9 +1,9 @@
 # variables that will be imported in the main app
 
-# token = '829399417:AAEZKjtE05GTzEdbKmmeAjTny8JXxkFHcUw'
-# username = 't.me/myCovidStatbot'
-# url = 'https://mycovidstat.herokuapp.com/'
+# token = '<bot token>'
+# username = '<bot username>'
+# url = '<server>'
 
-bot_token = "829399417:AAEZKjtE05GTzEdbKmmeAjTny8JXxkFHcUw"
-bot_user_name = "t.me/myCovidStatbot"
-URL = "https://mycovidstat.herokuapp.com/"
+bot_token = '<bot token>'
+bot_user_name = '<bot username>'
+URL = '<server>'
